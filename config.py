f8747327f3b7368e537a983a4a8310b30df978bb
ipetrash <ilya.petrash@inbox.ru>
2020-08-05 21:43:19 +0500

    Init

---
+++ config.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+TOKEN = '<TOKEN>'
+
+DIR_IMAGES = 'images'
+ERROR_TEXT = '⚠ Возникла какая-то проблема. Попробуйте повторить запрос или попробовать чуть позже...'
